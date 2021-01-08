954bc2db59e8c97699f5ed32cf76420c01e61c34
WitcherSilver <68162122+WitcherSilver@users.noreply.github.com>
2021-01-08 10:53:06 -0600

    FIST

---
+++ Data/Scripts/SupernovaMovement.lua
@@ -0,0 +1,7 @@
+﻿local Fist = script:GetCustomProperty("Fist"):WaitForObject()
+local Ease3D = require(script:GetCustomProperty("Ease3D"))
+
+--play when the ability is spawned
+Ease3D.EaseScale(Fist, Vector3.New(1), 3, Ease3D.EasingEquation.SINE, Ease3D.EasingDirection.INOUT)
+Ease3D.EasePosition(Fist, Vector3.New(0, 0, 0), 3, Ease3D.EasingEquation.QUINTIC, Ease3D.EasingDirection.IN)
+Ease3D.EaseRotation(Fist, Rotation.New(0, 0, 700), 3, Ease3D.EasingEquation.CUBIC, Ease3D.EasingDirection.IN)
