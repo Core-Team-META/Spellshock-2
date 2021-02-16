c5293f5dd2bae954b3409a5b961b0a5f49f30369
Brent Dunn <brent075@gmail.com>
2021-02-16 15:38:27 -0800

    Maint: Commented GamePlayStats & Updated Date Headers in Constants / Storage manager

--- Data/Scripts/META_GamePlayStats_Server.lua
+++ Data/Scripts/META_GamePlayStats_Server.lua
@@ -1,21 +1,36 @@
+------------------------------------------------------------------------------------------------------------------------
+-- REQUIRES
+------------------------------------------------------------------------------------------------------------------------
 local CONST = require(script:GetCustomProperty("CONST"))
 local GAME_STATE_API = require(script:GetCustomProperty("APIBasicGameState"))
-
+------------------------------------------------------------------------------------------------------------------------
+-- OBJECTS
+------------------------------------------------------------------------------------------------------------------------
 local GAME_STATE = script:GetCustomProperty("BasicGameStateManagerServer"):WaitForObject()
+------------------------------------------------------------------------------------------------------------------------
+-- CONSTANTS
+------------------------------------------------------------------------------------------------------------------------
+local LAST_GAME_WEIGHT = 0.1 -- Higher value means the last match is weight more towards overall weightedWins
+local TOTAL_GAME_WON_WEIGHT = 0.9 -- Higher value means the total games won is weight more towards overall weightedWins
+------------------------------------------------------------------------------------------------------------------------
+-- LOCAL FUNCTIONS
+------------------------------------------------------------------------------------------------------------------------
 
 local function CalculatePlayerGamesWon(player)
     local totalGamesPlayed = player:GetResource(CONST.TOTAL_GAMES)
     local totalGamesWon = player:GetResource(CONST.GAMES_WON)
-    local lastGameWon = player.serverUserData.LastMatchWon and 1 or 0
-    local weightMovingAverage = (lastGameWon * 0.10) + (totalGamesWon * 0.90) / totalGamesPlayed
+    local lastGameResult = player.serverUserData.IsLastMatchAWin and 1 or 0
+    local weightMovingAverage = (lastGameResult * LAST_GAME_WEIGHT) + (totalGamesWon * TOTAL_GAME_WON_WEIGHT) / totalGamesPlayed
     player.serverUserData.WeightedWins = weightMovingAverage
 end
+
 
 local function SetPlayerWeightedWins()
     for _, player in ipairs(Game.GetPlayers()) do
         CalculatePlayerGamesWon(player)
     end
 end
+
 
 --@param object player
 --@param int orcScore
@@ -24,14 +39,15 @@ end
 local function SetPlayerWinLoss(player, orcScore, elfScore)
     if orcScore > elfScore and player.team == CONST.TEAM.ORC then
         player:AddResource(CONST.GAMES_WON, 1)
-        player.serverUserData.LastMatchWon = true
+        player.serverUserData.IsLastMatchAWin = true
     elseif orcScore < elfScore and player.team == CONST.TEAM.ELF then
         player:AddResource(CONST.GAMES_WON, 1)
-        player.serverUserData.LastMatchWon = false
+        player.serverUserData.IsLastMatchAWin = false
     end
     player:AddResource(CONST.TOTAL_GAMES, 1)
-    player.serverUserData.LastMatchWon = false
+    player.serverUserData.IsLastMatchAWin = false
 end
+
 
 local function CalculateGamePlayStats()
     local orcScore = Game.GetTeamScore(CONST.TEAM.ORC)
@@ -40,6 +56,10 @@ local function CalculateGamePlayStats()
         SetPlayerWinLoss(player, orcScore, elfScore)
     end
 end
+
+------------------------------------------------------------------------------------------------------------------------
+-- GLOBAL FUNCTIONS
+------------------------------------------------------------------------------------------------------------------------
 
 function OnGameStateChanged(object, string)
     if string == "State" then
@@ -53,4 +73,7 @@ function OnGameStateChanged(object, string)
     end
 end
 
+------------------------------------------------------------------------------------------------------------------------
+-- LISTENERS
+------------------------------------------------------------------------------------------------------------------------
 GAME_STATE.networkedPropertyChangedEvent:Connect(OnGameStateChanged)
