54f3414bf3e9de34a038cb3e41c0196ee618d721
Brent Dunn <brent075@gmail.com>
2021-02-17 17:18:20 -0800

    Added: 4 New Achievements, expanded what game objects can count towards an achievement

--- Data/Scripts/ACHIEVEMENT_Server.lua
+++ Data/Scripts/ACHIEVEMENT_Server.lua
@@ -4,7 +4,17 @@
 -- Date: 2021/2/15
 -- Version 0.1.1
 ------------------------------------------------------------------------------------------------------------------------
+------------------------------------------------------------------------------------------------------------------------
+-- REQUIRES
+------------------------------------------------------------------------------------------------------------------------
+local GAME_STATE_API = require(script:GetCustomProperty("APIBasicGameState"))
+local CONST = require(script:GetCustomProperty("MetaAbilityProgressionConstants_API"))
+
+------------------------------------------------------------------------------------------------------------------------
+-- OBJECTS
+------------------------------------------------------------------------------------------------------------------------
 local ACHIEVEMENT_LIST = script:GetCustomProperty("Achievement_List"):WaitForObject()
+local GAME_STATE = script:GetCustomProperty("BasicGameStateManagerServer"):WaitForObject()
 local ACH_API
 while not _G.META_ACHIEVEMENTS do
     Task.Wait()
@@ -62,26 +72,34 @@ function PlayerDamaged(player, target, type, isHeadShot)
                 AddProgress(player, "HEADSHOT", 1)
             end
             target.serverUserData.ACH_killCredited = true
+            target.serverUserData.ACH_diedInRound = true
         end
     end
 end
 
-function OnKillStreak(player, killStreak)
-    if player:GetResource("AS_10KS") < killStreak then
-        SetProgress(player, "AS_10KS", killStreak)
+function OnKillStreak(player, value)
+    if player:GetResource("AS_10KS") < value then
+        SetProgress(player, "AS_10KS", value)
     end
 end
 
-function OnPlayerKill(player, killCount)
-    AddProgress(player, "AS_25KILL", killCount)
+function OnPlayerKill(player, value)
+    player.serverUserData.ACH_killCount = player.serverUserData.ACH_killCount and player.serverUserData.ACH_killCount + 1 or 1
+    print(player.serverUserData.ACH_killCount)
+    AddProgress(player, "AS_25KILL", value)
+    AddProgress(player, "AS_5KILL", value)
 end
 
-function OnPlayerDamage(player, damageAmount)
-    AddProgress(player, "AS_50kDMG", damageAmount)
+function OnPlayerDamage(player, value)
+    AddProgress(player, "AS_50kDMG", value)
 end
 
-function OnPlayerHealing(player, healAmount)
-    AddProgress(player, "AS_25KHEAL", healAmount)
+function OnPlayerHealing(player, value)
+    AddProgress(player, "AS_25KHEAL", value)
+end
+
+function OnPlayerCapture(player, value)
+    AddProgress(player, "AS_CAP1", value)
 end
 
 function OnPlayerRespawn(player)
@@ -117,16 +135,54 @@ function OnRewardCollected(player, id)
     ACH_API.CollectReward(player, id)
 end
 
+function OnRoundEnd()
+    local orcScore = Game.GetTeamScore(CONST.TEAM.ORC)
+    local elfScore = Game.GetTeamScore(CONST.TEAM.ELF)
+    for _, player in ipairs(Game.GetPlayers()) do
+
+        if
+            (orcScore > elfScore and player.team == CONST.TEAM.ORC) or
+                (orcScore < elfScore and player.team == CONST.TEAM.ELF)
+         then
+            AddProgress(player, "AS_100WINS", 1)
+        end
+
+        AddProgress(player, "AS_500MATCHES", 1)
+        if player.serverUserData.ACH_killCount and player.serverUserData.ACH_killCount >= 1 and not player.serverUserData.ACH_diedInRound then
+            AddProgress(player, "AS_UNKILLABLE", 1)
+        end
+
+        player.serverUserData.ACH_killCount = 0
+        player.serverUserData.ACH_diedInRound = false
+    end
+end
+
 ------------------------------------------------------------------------------------------------------------------------
 -- GLOBAL FUNCTIONS
 ------------------------------------------------------------------------------------------------------------------------
+
+function OnGameStateChanged(object, string)
+    if string == "State" then
+        local state = object:GetCustomProperty(string)
+        if state == GAME_STATE_API.GAME_STATE_ROUND_END then
+            OnRoundEnd()
+        end
+    end
+end
+
+------------------------------------------------------------------------------------------------------------------------
+-- LISTENERS
+------------------------------------------------------------------------------------------------------------------------
 Game.playerJoinedEvent:Connect(OnPlayerJoined)
 Game.playerLeftEvent:Connect(OnPlayerLeft)
+GAME_STATE.networkedPropertyChangedEvent:Connect(OnGameStateChanged)
+
 Events.Connect("AS.PlayerDamaged", PlayerDamaged) -- passes (player, target, type, isHeadShot)
 Events.Connect("AS.KillStreak", OnKillStreak) -- passes (player, currentKillStreak)
 Events.Connect("AS.LifeTimeKill", OnPlayerKill)
 Events.Connect("AS.LifeTimeDamage", OnPlayerDamage)
 Events.Connect("AS.LifeTimeHealing", OnPlayerHealing)
+Events.Connect("AS.PlayerPointCapture", OnPlayerCapture)
 Events.ConnectForPlayer("AS.RewardClaim", OnRewardCollected)
 --Events.Connect("CombatWrapAPI.OnDamageTaken", OnDamageTaken)
 Int()
