e4f50119ac8f3d36a863df1af54372eb6221fcda
BuckmonsterCORE <58750748+BuckmonsterCORE@users.noreply.github.com>
2021-03-26 15:32:26 -0700

    Adding gold for kill

--- Data/Scripts/KillFeedbackServer.lua
+++ Data/Scripts/KillFeedbackServer.lua
@@ -2,12 +2,13 @@
 	Kill Feedback UI
 	v1.0
 	by: standardcombo
-	
 	Shows a red skull each time you get a kill. Shows kill spree number in a text under the skull.
 --]]
 
 function OnPlayerDied(player, dmg)
 	if Object.IsValid(dmg.sourcePlayer) then
+		-- TEMP for demo
+		dmg.sourcePlayer:AddResource("GOLD", 50)
 		Events.BroadcastToPlayer(dmg.sourcePlayer, "KillFeedback")
 	end
 end
