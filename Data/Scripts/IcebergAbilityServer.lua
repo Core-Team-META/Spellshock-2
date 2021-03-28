6852166deae0404440be3bd4c01b320a4bd92b58
beneilts <36256880+beneilts@users.noreply.github.com>
2021-03-28 12:17:58 -0700

    Iceberg: added a small delay before the player is allowed to break out

--- Data/Scripts/IcebergAbilityServer.lua
+++ Data/Scripts/IcebergAbilityServer.lua
@@ -88,10 +88,12 @@ function OnSpecialAbilityExecute(thisAbility)
 	CurrentIceCube:SetWorldScale(Vector3.New( CoreMath.Round(DamageRadius / DEFAULT_DamageRadius, 3) ))
 	CurrentIceCube:AttachToPlayer(thisAbility.owner, "root")	
 	goingToTakeDamageListener = Events.Connect("CombatWrapAPI.GoingToTakeDamage", OnGoingToTakeDamage)
-	BindingPressedEvent = thisAbility.owner.bindingPressedEvent:Connect( OnBindingPressed )
-
 	Timer = META_AP().GetAbilityMod(SpecialAbility.owner, META_AP().T, "mod3", DEFAULT_Duration, SpecialAbility.name..": Duration")
 	damageTimer = 0
+	Task.Wait(0.5)
+	if thisAbility.owner and Object.IsValid(thisAbility.owner) and not thisAbility.owner.isDead then
+		BindingPressedEvent = thisAbility.owner.bindingPressedEvent:Connect( OnBindingPressed )	
+	end
 end
 
 function OnSpecialAbilityCooldown(thisAbility)
