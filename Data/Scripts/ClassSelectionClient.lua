040cb7a2ecd040f52f3ac89a13089a5b000a46ac
beneilts <36256880+beneilts@users.noreply.github.com>
2021-04-05 16:14:01 -0700

    Error fix: ClassSelectionClient

--- Data/Scripts/ClassSelectionClient.lua
+++ Data/Scripts/ClassSelectionClient.lua
@@ -815,7 +815,8 @@ function isAllowed(delay)
 end
 
 function Tick()
-	if ABGS.GetGameState() == ABGS.GAME_STATE_LOBBY and _G.CurrentMenu == _G.MENU_TABLE["ClassSelection"] and ABGS.GetTimeRemainingInState() and ABGS.GetTimeRemainingInState() < 2 then
+	if ABGS.GetGameState() == ABGS.GAME_STATE_LOBBY and _G.CurrentMenu == _G.MENU_TABLE["ClassSelection"] and ABGS.GetTimeRemainingInState() and ABGS.GetTimeRemainingInState() < 2 
+	and CurrentClassButton then
 		-- Auto select currently viewed class
 		
 		ClassSelectionCanvas.visibility = Visibility.FORCE_OFF
