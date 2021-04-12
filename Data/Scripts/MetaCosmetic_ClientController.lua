f4f45b85ddb44c2435d59414e66caa376e1eb498
Brent Dunn <brent075@gmail.com>
2021-04-11 18:27:23 -0700

    Maint: Added nil check to cosmetic data

--- Data/Scripts/MetaCosmetic_ClientController.lua
+++ Data/Scripts/MetaCosmetic_ClientController.lua
@@ -71,6 +71,11 @@ function OnChildAdded(parent, object)
     end
 end
 
+local function CosmeticTableIsValid(player, class, team, skin, bind)
+    return cosmeticTable[class] and cosmeticTable[class][player.team] and cosmeticTable[class][player.team][skin] and
+        cosmeticTable[class][player.team][skin][bind]
+end
+
 ------------------------------------------------------------------------------------------------------------------------
 -- Public Server API
 ------------------------------------------------------------------------------------------------------------------------
@@ -119,7 +124,7 @@ function API.GetCurrentCosmetic(player, bind, class)
     if skinId == 0 then
         skinId = 1
     end
-    return cosmeticTable[class][player.team][skinId][bind]
+    return CosmeticTableIsValid(player, class, player.team, skinId, bind) and cosmeticTable[class][player.team][skinId][bind] or 1
 end
 
 --@param object player
@@ -130,7 +135,7 @@ function API.GetCosmeticMuid(player, class, team, skin, bind)
         Task.Wait()
     end
     Task.Wait()
-    return cosmeticTable[class][team][skin][bind]
+    return CosmeticTableIsValid(player, class, player.team, skin, bind) and cosmeticTable[class][team][skin][bind] or 1
 end
 
 --@param object player
