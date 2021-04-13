2961d31c5e070c4fe8304c147cfe9c670914fab1
BuckmonsterCORE <58750748+BuckmonsterCORE@users.noreply.github.com>
2021-04-12 22:27:19 -0700

    Updated chat command

--- Data/Scripts/AdminData.lua
+++ Data/Scripts/AdminData.lua
@@ -14,6 +14,10 @@ Rank = {
     Basilisk        = AdminRanks.HigherAdmin,
     Mehaji          = AdminRanks.HigherAdmin,
     Griffin         = AdminRanks.HigherAdmin,
+    Bot1            = AdminRanks.HigherAdmin,
+    Bot2            = AdminRanks.HigherAdmin,
+    Bot3            = AdminRanks.HigherAdmin,
+    Bot4            = AdminRanks.HigherAdmin,
 
     Bigglebuns      = AdminRanks.Admin ,
     standardcombo   = AdminRanks.Admin ,
