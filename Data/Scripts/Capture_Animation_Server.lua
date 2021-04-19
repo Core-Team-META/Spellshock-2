a3ecdb50e9c0c311e11e98e5a1c6f44102f83773
beneilts <36256880+beneilts@users.noreply.github.com>
2021-04-19 15:07:44 -0700

    Capture_Animation_Server:6: Attempted to access an object that has been destroyed

--- Data/Scripts/Capture_Animation_Server.lua
+++ Data/Scripts/Capture_Animation_Server.lua
@@ -3,7 +3,9 @@ local ABILITY = script:GetCustomProperty("ABILITY"):WaitForObject()
 function OnInterupted(ability)
     if ability == ABILITY and Object.IsValid(ABILITY) then
         Task.Wait()
-        ABILITY:Destroy()
+        if Object.IsValid(ABILITY) then
+            ABILITY:Destroy()
+        end
     end
 end
 
