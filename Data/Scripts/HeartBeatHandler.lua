ed0210144870137cc777c7f99d73806f8099bac1
beneilts <36256880+beneilts@users.noreply.github.com>
2021-04-22 12:31:16 -0700

    HeartBeatHandler: setup alternate audio

--- Data/Scripts/HeartBeatHandler.lua
+++ Data/Scripts/HeartBeatHandler.lua
@@ -1,9 +1,14 @@
 local ABGS = require(script:GetCustomProperty("APIBasicGameState"))
 local HeartbeatSFX = script:GetCustomProperty("HeartbeatSFX"):WaitForObject()
 local DynamicCapturePoints = script:GetCustomProperty("DynamicCapturePoints"):WaitForObject()
+local AlternateSFX = script:GetCustomProperty("AlternateSFX"):WaitForObject()
 
+local LOCAL_PLAYER = Game.GetLocalPlayer()
 local interval = 1.5
 local loopTask = nil
+
+local alternateAudio = {}
+local noAudio = {}
 
 function OnGameStateChanged(oldState, newState)
     if (newState == ABGS.GAME_STATE_ROUND_END or newState == ABGS.GAME_STATE_PLAYER_SHOWCASE) and loopTask then
@@ -17,12 +22,18 @@ function OnTeamScoreChanged(team)
     local scoreLimit = DynamicCapturePoints:GetCustomProperty("ScoreLimit")
     if teamScore >= (scoreLimit-30) and not loopTask and ABGS.GAME_STATE_ROUND == ABGS.GetGameState() then
         loopTask = Task.Spawn(function ()
-            HeartbeatSFX:Play()
+            if alternateAudio[LOCAL_PLAYER.name] then
+                AlternateSFX:Play()
+            else
+                HeartbeatSFX:Play()
+            end
         end)
         loopTask.repeatCount = -1
         loopTask.repeatInterval = interval
     end
 end
 
-Game.teamScoreChangedEvent:Connect(OnTeamScoreChanged)
-Events.Connect("GameStateChanged", OnGameStateChanged)
+if not noAudio[LOCAL_PLAYER.name] then
+    Game.teamScoreChangedEvent:Connect(OnTeamScoreChanged)
+    Events.Connect("GameStateChanged", OnGameStateChanged)
+end
