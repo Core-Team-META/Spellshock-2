cc62a9d55a5f42853301e24b36e79a4957576a9b
Brent Dunn <brent075@gmail.com>
2021-05-04 16:08:47 -0700

    [Tournament] Added V1 Tournament logic

--- Spellshock2-Tournament/Data/Scripts/TournamentServer.lua
+++ Spellshock2-Tournament/Data/Scripts/TournamentServer.lua
@@ -13,49 +13,110 @@ local EVENT_ID = script:GetCustomProperty("EventID")
 
 local ADDITIONAL_DATA = require(script:GetCustomProperty("AdditionalData"))
 
-local MIN_PLAYERS_TO_SUBMIT = 2   -- 6
-local POINTS_PER_SUICIDE = -5    -- 0
-local POINTS_PER_KILL_WILD = 5   -- 20
-local POINTS_PER_KILL_TO_DEFENDER = 10  -- 30
-local POINTS_PER_KILL_WHILE_SUPPORT = 15   -- 25
-local POINTS_PER_DEATH_WILD = -4    -- -15
-local POINTS_PER_DEATH_WHILE_SUPPORT = -2    -- -12
+local MIN_PLAYERS_TO_SUBMIT = 2 -- 6
+local POINTS_PER_SUICIDE = 0 -- -5
+local POINTS_PER_KILL_WILD = 20 -- 5
+local POINTS_PER_ASSIST = 10
+
+local POINTS_PER_KILL_TO_DEFENDER = 30 -- 10
+local POINTS_PER_KILL_WHILE_SUPPORT = 25 -- 15
+local POINTS_PER_DEATH_WILD = -15 -- -4
+local POINTS_PER_DEATH_WHILE_SUPPORT = -12 -- -2
 --local POINTS_PER_HEADSHOT = 2
 
-local POINTS_PER_DAMAGE_DONE = 0.1    -- .55
-local POINTS_PER_HEALING_DONE = 0.075    -- .375
-local POINTS_PER_KILL_STREAK = 50   -- 100
+local POINTS_PER_DAMAGE_DONE = 0.55 -- .1
+local POINTS_PER_HEALING_DONE = 0.375 -- .075
+local POINTS_PER_KILL_STREAK = 100 -- 50
 
-local DIMINISHING_RETURNS = 1   -- 2
-local MIN_KILL_VALUE = 1   -- 3
-local POINTS_PER_OBJECTIVE_CAPTURED = 250   -- 200
-local POINTS_PER_OBJECTIVE_SUPPORT = 200    -- 200
-local BONUS_MULTIPLY_PER_UNIQUE_KILL = 0.109
+local DIMINISHING_RETURNS = 2 -- 1
+local MIN_KILL_VALUE = 3 -- 1
+local POINTS_PER_OBJECTIVE_CAPTURED = 200 -- 200
+local POINTS_PER_OBJECTIVE_SUPPORT = 200 -- 200
+local BONUS_MULTIPLY_PER_UNIQUE_KILL = 100
 local MAX_UNIQUE_COUNT = 8
 
---To Add
--- Kill Assists?
--- Score Differential (raw points added based on your team's score)
--- For example:
--- Orc Score = 500, Elf Score = 235. Orc team would get +500 points, Elf team would get +235 points. Adds to score after win multiplier calculation.
--- Make kill multiplier additive not a multiplier on the score (base 100 per unique kill)
--- Show assists on scoreboard for capturing points (not just 'f' to capture points) Change caps/assists
--- Have per class modifier on score applied at end?
--- Add "soft cap" value which when reached. 2 new variables, soft cap point, and soft cap amount. Subtract this soft cap amount from base point value after the softcap point is reached.
--- For example:
--- If points per objective is set to 200, soft cap point = 5, soft cap amount = 50. Values would go: [200,200,200,200,200,150,100,50,0,0,0]
--- Stop awarding points after a specific time (30m)
--- Add assist captures to server printouts
--- Separate bonus kill multiplier from kill points (since it will be additive), and add kill points as a new printout.
--- V2 have a kill assist value 
+-- Base Point Removal Softcaps
+local SOFTCAP_KILLSTREAK_COUNT = 1
+local SOFTCAP_KILLSTREAK_REDUCTION = 20
 
+local SOFTCAP_OBJECTIVE_COUNT = 5
+local SOFTCAP_OBJECTIVE_REDUCTION = 50
 
+local SOFTCAP_ASSIST_KILL_COUNT = 5
+local SOFTCAP_ASSIST_KILL_REDUCTION = 2
+
+-- Percentage Removal Softcaps
+local SOFTCAP_KILL_COUNT = 10
+local SOFTCAP_KILL_REDUCTION = 0.5
+
+local SOFTCAP_DAMAGE_COUNT = 5000
+local SOFTCAP_DAMAGE_REDUCTION = 0.5
+
+local SOFTCAP_HEALING_COUNT = 5000
+local SOFTCAP_HEALING_REDUCTION = 0.5
 
 --Point Multiplier For Winning & Losing Team
 local POINTS_FOR_VICTORY = 5
 local POINTS_FOR_LOSS = 4
 
 local STORAGE_KEY = "TournamentSupport"
+
+local function TagAssistPlayer(player, target, attackData)
+	target.serverUserData.damageTable = target.serverUserData.damageTable or {}
+	target.serverUserData.damageTable[player] = target.serverUserData.damageTable[player] or {}
+	local assistTbl = target.serverUserData.damageTable[player]
+	if assistTbl.timer and assistTbl.timer < time() then
+		assistTbl.damage = assistTbl.damage or 0
+		assistTbl.damage = assistTbl.damage + attackData.damage.amount
+		assistTbl.timer = time() + 10
+	else
+		assistTbl.damage = attackData.damage.amount
+		assistTbl.timer = time() + 10
+	end
+	target.serverUserData.damageTable[player] = assistTbl
+end
+
+local function CalculateBaseSoftCap(value, base, count, reduction)
+	local roundSoftcap = 0
+	if value > count then
+		roundSoftcap = value - count
+	end
+	if roundSoftcap > 0 then
+		local postCapPoints = 0
+		for i = 1, roundSoftcap do
+			local afterReduction = base - (reduction * i)
+			postCapPoints = postCapPoints + CoreMath.Clamp(afterReduction, 0, base)
+		end
+		local preCapPoints = count * base
+		return CoreMath.Round(preCapPoints + postCapPoints)
+	else
+		return CoreMath.Round((value * base))
+	end
+end
+
+-- Percentage Removal Softcaps
+local function CalculatePercentageSoftCap(value, base, count, reduction)
+	local roundSoftcap = 0
+	if value > count then
+		roundSoftcap = value - count
+	end
+
+	if roundSoftcap > 0 then
+		local preCapPoints = count * base
+		local postCapPoints = (value - count) * (base * reduction)
+		return CoreMath.Round(preCapPoints + postCapPoints)
+	else
+		return CoreMath.Round((value * base))
+	end
+end
+
+local function CalculateSoftCap(value, base, count, reduction)
+	if reduction < 1 then
+		return CalculatePercentageSoftCap(value, base, count, reduction)
+	else
+		return CalculateBaseSoftCap(value, base, count, reduction)
+	end
+end
 
 function SubmitScore(player, score, totalKills, headshots, uniquePlayersKilled)
 	score = CoreMath.Round(score)
@@ -82,6 +143,10 @@ function OnPlayerDamaged(attackData)
 	if not target:IsA("Player") then
 		return
 	end
+
+	-- Assist Calculation
+	TagAssistPlayer(player, target, attackData)
+
 	if not target.isDead then
 		return
 	end
@@ -112,9 +177,9 @@ function OnPlayerDamaged(attackData)
 
 	local killPoints = POINTS_PER_KILL_WILD
 
-	if player.serverUserData.supportCapture or player.serverUserData.onStrikePoint then
+	if player.serverUserData.onCapturePoint or player.serverUserData.isCapturingPoint then
 		killPoints = POINTS_PER_KILL_WHILE_SUPPORT
-	elseif target.serverUserData.onStrikePoint then
+	elseif target.serverUserData.onCapturePoint then
 		killPoints = POINTS_PER_KILL_TO_DEFENDER
 	end -- Points for dying
 	--
@@ -127,7 +192,7 @@ function OnPlayerDamaged(attackData)
 	]] local deathPoints =
 		POINTS_PER_DEATH_WILD
 
-	if target.serverUserData.supportCapture or target.serverUserData.onStrikePoint then
+	if target.serverUserData.onCapturePoint or target.serverUserData.isCapturingPoint then
 		deathPoints = POINTS_PER_DEATH_WHILE_SUPPORT
 	end
 
@@ -143,6 +208,19 @@ function OnPlayerDamaged(attackData)
 	playerData.points = playerData.points + killPoints
 	targetData.points = targetData.points + deathPoints
 
+	-- Apply Assist Points
+	for assistPlayer, assist in pairs(target.serverUserData.damageTable) do
+		if
+			assistPlayer and Object.IsValid(assistPlayer) and assistPlayer ~= player and assistPlayer.team ~= target.team and
+				assist.timer and
+				assist.timer >= time()
+		 then
+			local assistPlayerData = assistPlayer.serverUserData.tournament
+			assistPlayerData.killAssists = assistPlayerData.killAssists + 1
+			assistPlayer.serverUserData.tournament = assistPlayerData
+		end
+	end
+
 	-- Track unique kills bonus
 	local targetId = target.id
 	playerData.uniqueKills[targetId] = true
@@ -156,6 +234,7 @@ function ClearData(player)
 	playerData.points = 0
 	playerData.totalKills = 0
 	playerData.headshots = 0
+	playerData.killAssists = 0
 	playerData.uniqueKills = {}
 
 	player.serverUserData.tournament = playerData
@@ -220,6 +299,7 @@ end
 
 function OnPlayerRespawn(player)
 	player.serverUserData.tournament.killCredited = false
+	player.serverUserData.damageTable = {}
 end
 
 function OnPlayerJoined(player)
@@ -277,41 +357,66 @@ function OnRoundEnded()
 
 		-- Round points
 		local roundPoints = 0
-
+		local teamScoreBonus = 0
 		if player.team == winningTeam then
 			roundPoints = CoreMath.Round(winningScore * POINTS_FOR_VICTORY)
+			teamScoreBonus = winningScore
 		else
 			roundPoints = CoreMath.Round(losingScore * POINTS_FOR_LOSS)
+			teamScoreBonus = losingScore
 		end
 		playerData.points = playerData.points + roundPoints
 
 		-- Damage Points
 		local roundDamage = player:GetResource(CONST.COMBAT_STATS.TOTAL_DAMAGE_RES)
-		local damagePoints = CoreMath.Round((roundDamage * POINTS_PER_DAMAGE_DONE))
+		local damagePoints =
+			CalculateSoftCap(roundDamage, POINTS_PER_DAMAGE_DONE, SOFTCAP_DAMAGE_COUNT, SOFTCAP_DAMAGE_REDUCTION)
 		playerData.points = playerData.points + damagePoints
 
 		-- Healing Points
 		local roundHealing = player:GetResource(CONST.COMBAT_STATS.TOTAL_HEALING_RES)
-		local healingPoints = CoreMath.Round((roundHealing * POINTS_PER_HEALING_DONE))
+		local healingPoints =
+			CalculateSoftCap(roundHealing, POINTS_PER_HEALING_DONE, SOFTCAP_HEALING_COUNT, SOFTCAP_HEALING_REDUCTION)
 		playerData.points = playerData.points + healingPoints
 
 		-- Killstreak Points
 		local roundKillStreak = player:GetResource(CONST.COMBAT_STATS.LARGEST_KILL_STREAK)
-		local killStreakPoints = (roundKillStreak * POINTS_PER_KILL_STREAK)
+		local killStreakPoints =
+			CalculateSoftCap(roundKillStreak, POINTS_PER_KILL_STREAK, SOFTCAP_KILLSTREAK_COUNT, SOFTCAP_KILLSTREAK_REDUCTION)
 		playerData.points = playerData.points + killStreakPoints
 
 		-- Capture Points
 		local objectivesCaptured = player:GetResource(CONST.COMBAT_STATS.TOTAL_CAPTURE_POINTS)
-		local objectivePoints = (objectivesCaptured * POINTS_PER_OBJECTIVE_CAPTURED)
+		local objectivePoints =
+			CalculateSoftCap(
+			objectivesCaptured,
+			POINTS_PER_OBJECTIVE_CAPTURED,
+			SOFTCAP_OBJECTIVE_COUNT,
+			SOFTCAP_OBJECTIVE_REDUCTION
+		)
 		playerData.points = playerData.points + objectivePoints
+
+		-- Assist Kill Points
+		local assistKillPoints =
+			CalculateSoftCap(playerData.killAssists, POINTS_PER_ASSIST, SOFTCAP_ASSIST_KILL_COUNT, SOFTCAP_ASSIST_KILL_REDUCTION)
+		playerData.points = playerData.points + assistKillPoints
 
 		-- Assist Capture Points
 		local objectivesSupported = player:GetResource(CONST.COMBAT_STATS.CAPTURE_ASSISTS)
-		local objectiveAssistPoints = (objectivesSupported * POINTS_PER_OBJECTIVE_SUPPORT)
+		local objectiveAssistPoints =
+			CalculateSoftCap(
+			objectivesSupported,
+			POINTS_PER_OBJECTIVE_SUPPORT,
+			SOFTCAP_OBJECTIVE_COUNT,
+			SOFTCAP_OBJECTIVE_REDUCTION
+		)
 		playerData.points = playerData.points + objectiveAssistPoints
 
 		-- Add Default 1k starting points
 		playerData.points = playerData.points + 1000
+
+		-- Add Teamscore To Final Score
+		playerData.points = playerData.points + teamScoreBonus
 
 		-- Unique kills bonus
 		local uniqueCount = 0
@@ -326,8 +431,7 @@ function OnRoundEnded()
 		end
 		local uniqueKillsBonus = 0
 		if uniqueCount > 0 then
-			uniqueKillsBonus = playerData.points * uniqueCount * BONUS_MULTIPLY_PER_UNIQUE_KILL
-			uniqueKillsBonus = uniqueKillsBonus
+			uniqueKillsBonus = uniqueCount * BONUS_MULTIPLY_PER_UNIQUE_KILL
 			playerData.points = CoreMath.Round(playerData.points + uniqueKillsBonus)
 		end
 
@@ -339,9 +443,11 @@ function OnRoundEnded()
 			print("Damage Points: " .. tostring(damagePoints))
 			print("Healing Points: " .. tostring(healingPoints))
 			print("Kill Streak Points: " .. tostring(killStreakPoints))
+			print("Kill Assist Points: " .. tostring(assistKillPoints))
 			print("Capture Points: " .. tostring(objectivePoints))
-			print("Assist Points: " .. tostring(objectiveAssistPoints))
+			print("Assist Capture Points: " .. tostring(objectiveAssistPoints))
 			print("Unique Kill Points: " .. tostring(uniqueKillsBonus))
+			print("Team Score Bonus: " .. tostring(teamScoreBonus))
 			print("Points Total: " .. tostring(playerData.points))
 			print("------------------------------------------")
 		end
