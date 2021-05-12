89f9da84e1865195af238258276a9cb09e1dd593
beneilts <36256880+beneilts@users.noreply.github.com>
2021-05-12 10:34:50 -0700

    [Live] [Arena] [Brawl] WraithStrikeAbilityServer:297: Ability:Activate() failed.

--- SS2-BrawlArena/Data/Scripts/WraithStrikeAbilityServer.lua
+++ SS2-BrawlArena/Data/Scripts/WraithStrikeAbilityServer.lua
@@ -292,7 +292,7 @@ end
 function GoOnShortCooldown()
 	if not SpecialAbility.owner or not Object.IsValid(SpecialAbility.owner) then return end
 	goingIntoShortCooldown = true
-	while Object.IsValid(SpecialAbility) and SpecialAbility:GetCurrentPhase() ~= AbilityPhase.COOLDOWN do
+	while Object.IsValid(SpecialAbility) and SpecialAbility.owner and SpecialAbility:GetCurrentPhase() ~= AbilityPhase.COOLDOWN do
 		if SpecialAbility:GetCurrentPhase() == AbilityPhase.READY then
 			SpecialAbility:Activate()
 		else
